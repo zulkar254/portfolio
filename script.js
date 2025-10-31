dd297f521e26fcaf1f1f04fab64a9780326dbb1a
Zulkar Naine Alam <84017711+zulkar254@users.noreply.github.com>
2025-10-31 17:53:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Typing animation
+const typed = new Typed(".typing", {
+  strings: [
+    "Java Backend Developer",
+    "Spring Boot Specialist",
+    "Microservices Architect",
+    "Docker & Kafka Enthusiast"
+  ],
+  typeSpeed: 80,
+  backSpeed: 50,
+  loop: true,
+});
